1e8ebedfff01b9ec6edac3fdfba23ef05849019d
Andrei Margert <margert95@gmail.com>
2020-10-08 11:00:23 +0200

    Remove test

--- test.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-logging.basicConfig(level=logging.INFO, filename='training.log', filemode='w', format='%(asctime)s - %(message)s')
-logging.info('message')
-logging.info('message')
-logging.info('message')
-logging.info('message')
-
